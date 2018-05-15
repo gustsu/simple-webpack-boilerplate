909b4a6de92cba4ebc477474b9d174af2e661cf7
Justin Tew <justin-@live.com>
2018-05-14 21:04:41 -0700

    added scss support

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # webpack4-starter
 https://hackernoon.com/a-tale-of-webpack-4-and-how-to-finally-configure-it-in-the-right-way-4e94c8e7e5c1
+
+
+##A quick description of how rules usually work:
+{
+        test: /\.YOUR_FILE_EXTENSION$/,
+        exclude: /SOMETHING THAT IS THAT EXTENSION BUT SHOULD NOT BE PROCESSED/,
+        use: {
+          loader: "loader for your file extension  or a group of loaders"
+        }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 // webpack v4
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
@@ -14,7 +15,18 @@ module.exports = {
         use: {
           loader: "babel-loader"
         }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
       }
     ]
-  }
-};
+  },
+  plugins: [ 
+    new ExtractTextPlugin({filename: 'style.css'})
+  ]
+};;
